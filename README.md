244f2b09a92f92c9f5acc320b3a2440e4dba0cc7
Destry <destrys@gmail.com>
2015-01-22 10:39:22 -0700

    most of run_analysis and the beginning of the README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # cleaningdata
+This repo contains a script to clean and summarize a dataset of activity measurements from a Samsung Galaxy S smartphone.
+
+## Initial Data
+The data used for this course project was downloaded from [this address](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip). And here is [the full description of the initial data](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+To run run_analysis.R the dataset needs be be downloaded and unzipped in this repository's main directory. The directory name should be "UCI HAR Dataset".
+
+## run_analysis.R
+The code to clean and summarize the intial data into a single tidy dataset is contained in run_analysis.R. 
+The following steps are performed in the code:
+*  Load and combine the data
+
+  Each observation is split into 3 file: the phone data, the subject number, and the activity number. 
+  Additionally, the data are split into two sets: training and test for six total files:
+  *  test/X_test.txt: test phone data
+  *  test/y_test.txt: test activity numbers
+  *  test/subject_test.txt: test subject numbers
+  *  train/X_train.txt: training phone data
+  *  train/y_train.txt: training activity numbers
+  *  train/subject_train: training subject numbers
+  For this project I combined the phone, subject, and activity data
+  into a dataframe by adding the subject and activity data as new columns. I then concatenated the rows of  the training and test datframes
+  to create a single dataframe
---
+++ run_analysis.R
@@ -0,0 +1,79 @@
+library(dplyr)
+library(reshape2)
+
+# Load and Merge Training and Test Data into a single dataframe
+## Load Test Data
+raw_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+activity_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+## Add Activity and Subject Data to Rest of Observations
+raw_test <- cbind(activity_test, subject_test, raw_test)
+
+## Load Train Data
+raw_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+activity_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+## Add Activity and Subject Data to Rest of Observations
+raw_train <- cbind(activity_train, subject_train, raw_train)
+
+## Combine Test and Train Data
+raw <- rbind(raw_test, raw_train)
+rm(raw_test)
+rm(raw_train)
+
+# Load and process Feature Names
+feature_names <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+feature_names <- c("activity_num", "subject", feature_names[,2])
+
+# Extract Indices of features to keep (mean and standard deviation measurements)
+feature_indx <- grep("[Mm]ean|std",feature_names)
+
+# Reform Feature Name List and Indices
+feature_names <- c("activity_num", "subject", feature_names[feature_indx])
+feature_indx <- c(1, 2, feature_indx)
+
+# Extract Mean and STD Measurements
+raw <- raw[,feature_indx]
+
+# Reformat Observation Names
+feature_names <- sub("^f","FFT_",feature_names)
+feature_names <- sub("^t","", feature_names)
+feature_names <- sub("^angle\\(","meanAngleBtwn__", feature_names)
+feature_names <- sub("Acc","Accel", feature_names)
+feature_names <- sub("BodyBody","Body", feature_names)
+feature_names <- sub("Body","Body_", feature_names)
+feature_names <- sub("Gravity","Gravity_", feature_names)
+feature_names <- gsub("-","_", feature_names)
+feature_names <- sub("Jerk","_Jerk", feature_names)
+feature_names <- sub("Mag","_Magnitude", feature_names)
+feature_names <- sub("_([XYZ])$","_\\1axis", feature_names)
+feature_names <- sub("tBody","Body", feature_names)
+feature_names <- gsub("\\(","", feature_names)
+feature_names <- gsub("\\)","", feature_names)
+feature_names <- sub("_meanFreq$","_weightedMean", feature_names)
+feature_names <- sub("_meanFreq_","_weightedMean_", feature_names)
+feature_names <- sub("([XYZ]),","\\1axis__and__", feature_names)
+feature_names <- sub("Mean,","_mean__and__", feature_names)
+feature_names <- sub("__gravity.*","__Gravity_mean", feature_names)
+feature_names <- sub("GravityAccel","Gravity_Accel", feature_names)
+
+# Set Feature Names in dataframe
+colnames(raw) <- feature_names
+
+# Load Activity Descriptions
+activity_names <- read.table("UCI HAR Dataset/activity_labels.txt",
+                             stringsAsFactors = FALSE,
+                             col.names = c("activity_num","activity"))
+
+# Replace Activity Numbers with Activity Names
+raw <- left_join(raw,activity_names)
+raw <- select(raw,-activity_num)
+raw <- mutate(raw, activity = as.factor(activity), subject = as.factor(subject))
+
+# Extract a mean value for each feature for each Activity/Subject pair.
+rawmelt <- melt(raw, id.vars = c("activity", "subject"))
+feature_averages <- dcast(rawmelt, activity + subject ~ variable, mean)
+
+# Print averages dataframe to a text file.
